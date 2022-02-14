4e7690e4b90b66ec9dcb740605184d6de8298e4a
Pedro Fiuza <pdrfiuza@gmail.com>
2022-02-14 01:06:17 -0300

    how to create scaffold cards

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
+        body: Column(
+          children: [
+            Card(
+                child: ListTile(
+              leading: Icon(Icons.local_hospital),
+              title: Text('Teste'),
+            )),
+            Card(
+                child: ListTile(
+              leading: Icon(Icons.local_hospital),
+              title: Text('teste2'),
+            )),
+          ],
+        ),
         appBar: AppBar(
           title: Text('Transferencias'),
         ),
